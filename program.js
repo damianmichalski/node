89770fc0c748c17a968229826e51fceb11361dee
DamianM <damian.michalski@kramp.com>
2018-12-05 09:59:17 +0100

    first node.js

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            case '/env':
+                console.log(process.env);
+                break;
+            case '/version':
+                console.log(process.versions);
+                break;
+            default:
+                process.stderr.write('Wrong instruction!');
+        }
+    }
+});
