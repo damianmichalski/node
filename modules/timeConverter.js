20586ffc75ad30dc97fd7a485e5c0f25364deebf
DamianM <damian.michalski@kramp.com>
2018-12-19 09:27:44 +0100

    node.js

--- modules/timeConverter.js
+++ modules/timeConverter.js
@@ -1,5 +1,4 @@
 function getTimeConverter(uptime) {
-    return ((uptime / 3600).toFixed(0))  + ' godzin '  + ((Math.floor((uptime % 3600) / 60)).toFixed(0)) +  ' min. ' +  ((Math.floor(uptime % 60))) +  ' sek. ';
+    return (uptime / 3600).toFixed(0)  + ' godzin '  + (Math.floor((uptime % 3600) / 60)) +  ' min. ' +  (Math.floor(uptime % 60)) +  ' sek. ';
 }
-
 exports.uptimeSystem = getTimeConverter;
